ef88685cbd80f88c9bb2a694cfbc4ef468a6f2ed
Adhy <adhywiranatap@gmail.com>
2018-03-23 22:40:27 +0700

    add play, pause, and playback

--- src/index.js
+++ src/index.js
@@ -7,22 +7,34 @@ import webPlayer from './webPlayer';
 export default class WistiaPlayer extends PureComponent {
   state = {
     isLoading: true,
+    isPlaying: false,
   }
   
   static propTypes = {
     videoId: PropTypes.any,
   }
 
+  togglePlaying = (command) => {
+    this.webview.postMessage(command);
+  }
+
+  setPlaybackSpeed = (speed) => {
+    this.webview.postMessage(`speed:${speed}`);
+  }
+
   render() {
     return (
       <WebView
-        style={{ flex: 1 }}
+        ref={webview => { this.webview = webview; }}
+        style={{ flex: 1, ...this.props.style }}
         javaScriptEnabled={true}
         mediaPlaybackRequiresUserAction={false}
         builtInZoomControls={false}
         allowsInlineMediaPlayback={true}
+        scalesPageToFit={false}
         scrollEnabled={false}
         bounces={false}
+        onMessage={this.props.onMessage}
         source={{
           html: webPlayer(this.props.videoId),
           baseUrl: 'https://wistia.com',
--- src/webPlayer.js
+++ src/webPlayer.js
@@ -3,9 +3,49 @@ export default videoId => `
   <html style="height: 100%; width: 100%; padding: 0; margin:0;">
     <head>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <style>
+        html, body {
+          overflow-x: hidden;
+          overflow-y: hidden;
+          padding: 0; margin: 0;
+        }
+        body {
+          position: relative;
+          padding: 0; margin: 0;
+          background-color: black;
+        }
+      </style>
     </head>
-    <body style="padding: 0; margin: 0;">
-      <script src="https://fast.wistia.com/embed/medias/${videoId}.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:177.92% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_3r2k7bdc2d seo=false videoFoam=true" style="height:100%;width:100%">&nbsp;</div></div></div>
+    <body>
+      <div style="background: transparent; width: 100%; height: 100%; position: fixed; top: 0; left: 0; z-index: 995"></div>
+      
+      <script src="https://fast.wistia.com/embed/medias/${videoId}.jsonp" async></script>
+      <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+      <div class="wistia_responsive_padding" style="padding:177.92% 0 0 0;position:relative;">
+        <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
+          <div class="wistia_embed wistia_async_3r2k7bdc2d seo=false videoFoam=true" style="height:100%;width:100%">&nbsp;</div>
+        </div>
+      </div>
+      <script>
+
+      window._wq = window._wq || [];
+      _wq.push({ id: "${videoId}", onReady: function(video) {
+        document.addEventListener("message", function(data) {
+          let command = data.data;
+          let speed = 1;
+          if(command.includes('speed')) {
+            speed = Number(command.split(':')[1]) || 1;
+            command = 'speed';
+          }
+
+          switch(command) {
+            case 'play': video.play(); break;
+            case 'pause': video.pause(); break;
+            case 'speed': video.playbackRate(speed); break;
+          }
+        });
+      }});
+      </script>
     </body>
   </html>
 `;
