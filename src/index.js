d00a7b9dcc156f7c96bee6d01918399486e1d04e
Adhy <adhywiranatap@gmail.com>
2018-03-23 10:36:00 +0700

    update web content for testing

--- src/index.js
+++ src/index.js
@@ -17,9 +17,18 @@ export default class WistiaPlayer extends PureComponent {
           source={{html: webPlayer(this.props.videoId), baseUrl: 'https://wistia.com' }}
           style={{marginTop: 20, height: 300, width: 300 }}
         /> */}
-        <WebView
+        {/* <WebView
           source={{html: 'arigatou gozaimazu!!!', baseUrl: 'https://wistia.com' }}
           style={{marginTop: 20, height: 300, width: 300 }}
+        /> */}
+        <WebView
+          style={{flex: 1 }}
+          javaScriptEnabled={true}
+          mediaPlaybackRequiresUserAction={false}
+          builtInZoomControls={false}
+          allowsInlineMediaPlayback={true}
+          scrollEnabled={false}
+          source={{uri: 'https://adoring-engelbart-8e45c7.netlify.com/' }}
         />
       </View>
     );
